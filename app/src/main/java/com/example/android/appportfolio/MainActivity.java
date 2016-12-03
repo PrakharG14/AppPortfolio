068da0b18d77f90bb52db16a232a14d2b32c551a
Gairola <gairolap@f45c89be074b.ant.amazon.com>
2016-12-03 19:55:45 +0530

    Made code modular

--- app/src/main/java/com/example/android/appportfolio/MainActivity.java
+++ app/src/main/java/com/example/android/appportfolio/MainActivity.java
@@ -15,62 +15,33 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Handle on-click event on Popular Movies button
-     * @param view
+     * Handle on-click event for button
      */
-    public void onClickPopularMoviesButton(View view) {
+    public void onButtonClick(View view) {
         Context context = getApplicationContext();
-        CharSequence text = "This button will launch Popular Movies";
-        Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
-    }
-
-    /**
-     * Handle on-click event on Stock Hawk button
-     * @param view
-     */
-    public void onClickStockHawk(View view) {
-        Context context = getApplicationContext();
-        CharSequence text = "This button will launch Stock Hawk";
-        Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
-    }
-
-    /**
-     * Handle on-click event on Build It Bigger button
-     * @param view
-     */
-    public void onClicksBuildItBigger(View view) {
-        Context context = getApplicationContext();
-        CharSequence text = "This button will launch Build It Bigger";
-        Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
-    }
-
-    /**
-     * Handle on-click event on Your App Material button
-     * @param view
-     */
-    public void onClickMakeYourAppMaterial(View view) {
-        Context context = getApplicationContext();
-        CharSequence text = "This button will launch Make you App Material";
-        Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
-    }
-
-    /**
-     * Handle on-click event on Go Ubiquitous button
-     * @param view
-     */
-    public void onClickGoUbiquitous(View view) {
-        Context context = getApplicationContext();
-        CharSequence text = "This button will launch Go Ubiquitous";
-        Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
-    }
-
-    /**
-     * Handle on-click event on Capstone Project Button
-     * @param view
-     */
-    public void onClickCapstoneApp(View view) {
-        Context context = getApplicationContext();
-        CharSequence text = "This button will launch my Capstone Project";
+        CharSequence text;
+        switch (view.getId()) {
+            case R.id.button1:
+                text = getString(R.string.button1_desc);
+                break;
+            case R.id.button2:
+                text = getString(R.string.button2_desc);
+                break;
+            case R.id.button3:
+                text = getString(R.string.button3_desc);
+                break;
+            case R.id.button4:
+                text = getString(R.string.button4_desc);
+                break;
+            case R.id.button5:
+                text = getString(R.string.button5_desc);
+                break;
+            case R.id.button6:
+                text = getString(R.string.button6_desc);
+                break;
+            default:
+                return;
+        }
         Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
     }
 }
